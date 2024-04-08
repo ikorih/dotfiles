212b7d0e6130f877044b6681e61e991fe75959a2
Hiroki Fujikami <ikorih@protonmail.com>
2024-04-08 12:15:10 +0900

    feat(zsh): 🎸 fish設定ファイルの追加

--- fish/config.fish
+++ fish/config.fish
@@ -17,6 +17,24 @@ set -gx EDITOR nvim
 alias vim='nvim'
 
 
+# vi mode
+function fish_user_key_bindings
+    for mode in default insert visual
+        fish_default_key_bindings -M $mode
+    end
+    fish_vi_key_bindings --no-erase
+    if test "$__fish_active_key_bindings" = fish_vi_key_bindings
+        bind -M insert -m default jk force-repaint
+    end
+end
+
+function ide
+    tmux split-window -v -l 30%; and tmux split-window -h -l 66%; and tmux split-window -h -l 50%
+end
+
+funcsave ide
+
+
 switch (uname)
     case Darwin
         source (dirname (status --current-filename))/config.osx.fish
---
+++ fish/functions/ide.fish
@@ -0,0 +1,3 @@
+function ide
+    tmux split-window -v -l 30%; and tmux split-window -h -l 66%; and tmux split-window -h -l 50%
+end
