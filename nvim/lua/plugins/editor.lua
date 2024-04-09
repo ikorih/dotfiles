94943981f80611a4b358a99c3ba98287bfe75935
Hiroki Fujikami <ikorih@protonmail.com>
2024-04-09 18:23:20 +0900

    feat(neovim): 🎸 vim-wxpand-regionプラグインの導入: vを2回打つとテキスト選択

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -14,6 +14,32 @@ return {
   },
 
   {
+    "terryma/vim-expand-region",
+    event = "VeryLazy",
+    keys = {
+      { "v", mode = { "x" }, "<Plug>(expand_region_expand)", desc = "expand_region_expand" },
+      { "V", mode = { "x" }, "<Plug>(expand_region_shrink)", desc = "expand_region_shrink" },
+    },
+    init = function()
+      vim.cmd([[
+            let g:expand_region_text_objects = {
+            \ 'iw'  :0,
+            \ 'iW'  :0,
+            \ 'i"'  :0,
+            \ 'i''' :0,
+            \ 'i]'  :1,
+            \ 'ib'  :1,
+            \ 'iB'  :1,
+            \ 'il'  :1,
+            \ 'ii'  :1,
+            \ 'ip'  :0,
+            \ 'ie'  :0,
+            \ }
+            ]])
+    end,
+  },
+
+  {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
