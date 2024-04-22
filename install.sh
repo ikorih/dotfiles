25a998fe892589fb8d4a09e0adb26ea73ffb8d17
Hiroki Fujikami <ikorih@protonmail.com>
2024-04-22 15:59:38 +0900

    feat(開発ツール): 🎸 install.shにコマンド追加

--- install.sh
+++ install.sh
@@ -1,40 +1,51 @@
 # use colors on terminal
 if which tput >/dev/null 2>&1; then
-  ncolors=$(tput colors)
+	ncolors=$(tput colors)
 fi
 if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
-  RED="$(tput setaf 1)"
-  GREEN="$(tput setaf 2)"
-  YELLOW="$(tput setaf 3)"
-  BLUE="$(tput setaf 4)"
-  BOLD="$(tput bold)"
-  NORMAL="$(tput sgr0)"
+	RED="$(tput setaf 1)"
+	GREEN="$(tput setaf 2)"
+	YELLOW="$(tput setaf 3)"
+	BLUE="$(tput setaf 4)"
+	BOLD="$(tput bold)"
+	NORMAL="$(tput sgr0)"
 else
-  RED=""
-  GREEN=""
-  YELLOW=""
-  BLUE=""
-  BOLD=""
-  NORMAL=""
+	RED=""
+	GREEN=""
+	YELLOW=""
+	BLUE=""
+	BOLD=""
+	NORMAL=""
 fi
 
-
 usage() {
-  echo $YELLOW
-  cat <<\EOF
+	echo $YELLOW
+	cat <<\EOF
 Commands:
-	font-nerd (install Nerd Font)
-	quit
+  font-nerd (install Nerd Font)
+  setup-links (create symbolic links for Apple Silicon)
+  quit
 EOF
 	echo $NORMAL
 }
 
 nerd_fonts() {
-  git clone --branch=master --depth=1 https://github.com/ryanoasis/nerd-fonts.git
-  cd nerd-fonts
-  ./install.sh $1
-  cd ..
-  rm -rf nerd-fonts
+	git clone --branch=master --depth=1 https://github.com/ryanoasis/nerd-fonts.git
+	cd nerd-fonts
+	./install.sh $1
+	cd ..
+	rm -rf nerd-fonts
+}
+
+setup_links() {
+	ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+	ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+	ln -s ~/dotfiles/zsh/zshrc_local_mac_silicon ~/.zshrc_local
+	ln -s ~/dotfiles/nvim ~/.config/nvim
+	ln -s ~/dotfiles/fish ~/.config/fish
+	ln -s ~/dotfiles/lazygit ~/.config/lazygit
+	ln -s ~/dotfiles/changelog.config.js ~/changelog.config.js
+	echo "${GREEN}Symbolic links created successfully!${NORMAL}"
 }
 
 # main
@@ -42,24 +53,27 @@ nerd_fonts() {
 main() {
 	usage
 	echo -n "${BOLD}command: $NORMAL"
-  read command
-  case $command in
-    font-nerd)
-      nerd_fonts Meslo
-      main
-      ;;
-    quit)
-      echo "bye!"
-      exit 0
-      ;;
-    *)
-      echo "${RED}command not found.$NORMAL"
-      main
-      ;;
-  esac
+	read command
+	case $command in
+	font-nerd)
+		nerd_fonts Meslo
+		main
+		;;
+	setup-links)
+		setup_links
+		main
+		;;
+	quit)
+		echo "bye!"
+		exit 0
+		;;
+	*)
+		echo "${RED}command not found.$NORMAL"
+		main
+		;;
+	esac
 }
 
 main
 
 exit 0
-
