0511efb48b15d5ae2539bce00e60c4f9dfb6d06f
Hiroki Fujikami <ikorih@protonmail.com>
2024-09-08 18:27:15 +0900

    mod(開発ツール): 🛠 emmetの展開をctl+e+,に変更

--- nvim/lua/plugins/emmet.lua
+++ nvim/lua/plugins/emmet.lua
@@ -4,35 +4,35 @@ return {
     "mattn/emmet-vim",
     event = { "BufRead", "BufNewFile" },
     init = function()
-      vim.g.user_emmet_leader_key = "<C-f>" -- Control+f+,に設定
+      vim.g.user_emmet_leader_key = "<C-e>" -- Control+f+,に設定
     end,
   },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      -- @type lspconfig.options
-      servers = {
-        emmet_language_server = {
-          filetypes = {
-            "html",
-            "typescriptreact",
-            "javascriptreact",
-            "css",
-            "sass",
-            "scss",
-            "less",
-            "php",
-          },
-          init_options = {
-            html = {
-              options = {
-                -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L26
-                ["bem.enabled"] = true,
-              },
-            },
-          },
-        },
-      },
-    },
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     -- @type lspconfig.options
+  --     servers = {
+  --       emmet_language_server = {
+  --         filetypes = {
+  --           "html",
+  --           "typescriptreact",
+  --           "javascriptreact",
+  --           "css",
+  --           "sass",
+  --           "scss",
+  --           "less",
+  --           "php",
+  --         },
+  --         init_options = {
+  --           html = {
+  --             options = {
+  --               -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L26
+  --               ["bem.enabled"] = true,
+  --             },
+  --           },
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
 }
