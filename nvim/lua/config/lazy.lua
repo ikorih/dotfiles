e1505b2278b6bdd0020b19ef7372fe0149d20fc5
Hiroki Fujikami <ikorih@protonmail.com>
2024-12-18 11:39:46 +0900

    mod(neovim): 🛠 lazyvim設定更新

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -30,7 +30,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     -- import/override with your plugins
     { import = "plugins" },
